f476e12905e444680a4be783d74298643b2491f5
Fábio Rodrigues <fabio.info@gmail.com>
2019-01-11 17:51:51 -0200

    Fixed exception test

--- spec/exception_spec.rb
+++ spec/exception_spec.rb
@@ -3,14 +3,11 @@ require 'spec_helper'
 describe 'exception' do
 
   before(:all) do
-    ActiveRecord::Base.establish_connection(
-      adapter:  'firebird',
-      username: 'SYSDBA',
-      password: 'masterkey',
-      host: 'db',
-      database: '/firebird/data/example.fdb',
-      encoding: 'Windows-1252',
-    )
+    ActiveRecord::Base.establish_connection(ActiveRecord::Base.connection_config.merge(encoding: 'Windows-1252'))
+  end
+
+  after(:all) do
+    ActiveRecord::Base.establish_connection(ActiveRecord::Base.connection_config.merge(encoding: 'UTF-8'))
   end
 
   class Model < ActiveRecord::Base
