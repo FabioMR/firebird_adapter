c305d8a3e652a9281515cb04d87b0a94da1e2013
Fábio Rodrigues <fabio.info@gmail.com>
2019-01-25 16:35:03 -0200

    Fixed limit and offset bug for Rails < 5.2

--- lib/active_record/extensions.rb
+++ lib/active_record/extensions.rb
@@ -4,3 +4,12 @@ ActiveRecord::Calculations.module_eval do
     calculate(:count, column_name || 1)
   end
 end
+
+class ActiveRecord::ConnectionAdapters::AbstractAdapter
+  def combine_bind_parameters(from_clause: [], join_clause: [], where_clause: [], having_clause: [], limit: nil, offset: nil)
+    result = from_clause + join_clause + where_clause + having_clause
+    result.unshift(offset) if offset
+    result.unshift(limit) if limit
+    result  
+  end
+end if ActiveRecord::VERSION::STRING < '5.2.0'
