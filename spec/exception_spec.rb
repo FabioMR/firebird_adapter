ec1720f2e56790109353476f1953c46a963e7823
Fábio Rodrigues <fabio.info@gmail.com>
2021-06-14 10:13:17 -0300

    Changed connection_config to connection_db_config to remove deprecated message

--- spec/exception_spec.rb
+++ spec/exception_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe 'exception' do
 
   before(:all) do
-    @initial_encoding ||= ActiveRecord::Base.connection_config[:encoding] || ActiveRecord::ConnectionAdapters::FirebirdAdapter::DEFAULT_ENCODING
-    ActiveRecord::Base.establish_connection(ActiveRecord::Base.connection_config.merge(encoding: 'Windows-1252'))
+    @initial_encoding ||= ActiveRecord::Base.connection_db_config.configuration_hash[:encoding] || ActiveRecord::ConnectionAdapters::FirebirdAdapter::DEFAULT_ENCODING
+    ActiveRecord::Base.establish_connection(ActiveRecord::Base.connection_db_config.configuration_hash.merge(encoding: 'Windows-1252'))
   end
 
   after(:all) do
-    ActiveRecord::Base.establish_connection(ActiveRecord::Base.connection_config.merge(encoding: @initial_encoding))
+    ActiveRecord::Base.establish_connection(ActiveRecord::Base.connection_db_config.configuration_hash.merge(encoding: @initial_encoding))
   end
 
   it 'execute block with exception' do
